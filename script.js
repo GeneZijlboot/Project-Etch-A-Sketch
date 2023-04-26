64d8df4231a83cf0b38fffcde806ffd930474146
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-04-26 15:31:02 +0200

    user-input.value working with the console.log

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
-/declaring values
-const button = document.getElementById('button');
-const userinput = document.getElementById('input');
-
-//Grid now has the value of the user input
 function Grid() {
+    //declaring variables
+    const button = document.getElementById('button');
+    const userinput = document.getElementById('input');
+
     button.addEventListener("click", () => {
         if(userinput.value < 2){
             alert("ERROR, number to low");
-        } else if(userinput.value > 100){
+        }else if(userinput.value > 100){
             alert("ERROR! number to big");
-        } else {
-            console.log(userinput.value)
+        }else{
+            for(let x = 0; x < userinput.value; x++)
+            {   
+                console.log('correct babyyyy');
+                for(let y = 0; y < userinput.value; y++)
+                {
+                    console.log('correct babyyyy')
+                }
+            }
         }
     });
 }
 
-//function grid werkt + errors ook,
-//de waarde van de userinput is stored in "GridValue" en deze GridValue moet je vervolgens gebruiken om de grid te maken doormiddel van loopie denk ik..
-
-function GenerateGrid(){
-    const GridValue = Grid();
-    for(let x = 0; x < 16; x++)
-    {   
-        console.log('x');
-        for(let y = 0; y < 16; y++)
-        {
-            console.log('y')
-        }
-    }
-}
-
-GenerateGrid();
-
-/*
-}else{  
-    console.log(GridValue);
-}
-
-for(let i=0; i<GridValue; i++){
-    something..addEventListener("mouseover", => {
-        const square = document.createElement('div');
-        square
-    });
-}
-*/
+Grid();
