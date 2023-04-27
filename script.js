9e57d8411d129751b5974c31fe1a14b8f1dfc829
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-04-27 13:14:23 +0200

    almost done, but cant figure out how to print the squares to the screen...

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-function Grid() {
+//declaring variables
+const grid = document.querySelector('MainContainer');
+let squareSize = 8;
+
+GeneratingGrid(squareSize);
+
+//creating squared divs
+function SquareMaker(size){
+    const div = document.createElement('div')
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+
+    return div;
+}
+
+//creating the grid
+function GeneratingGrid() {
+
     //declaring variables
     const button = document.getElementById('button');
-    const userinput = document.getElementById('input');
+    let userinput = document.getElementById('input'); 
 
+    //declaring error message for less then 2 or bigger then 100
     button.addEventListener("click", () => {
         if(userinput.value < 2){
             alert("ERROR, number to low");
         }else if(userinput.value > 100){
             alert("ERROR! number to big");
         }else{
+            //creating the loop for building the grid
             for(let x = 0; x < userinput.value; x++)
             {   
-                console.log('correct babyyyy');
                 for(let y = 0; y < userinput.value; y++)
                 {
-                    console.log('correct babyyyy')
+                   console.log("sure fam!");
+                   grid.appendChild(SquareMaker(grid.clientWidth / userinput.value)); //?
                 }
             }
         }
     });
 }
-
-Grid();
