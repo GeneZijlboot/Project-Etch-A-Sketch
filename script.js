0f3bf3e97b2bc9f50a604f518418a5ebdd317c81
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-04-28 23:15:36 +0200

    almsot finished, a few bugs with the gridsize

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-let board = document.querySelector("MainContainer");
-board.style.gridTemplateColumns =  "repeat(16 , 1fr)";
-board.style.gridTemplateRows =  "repeat(16 , 1fr)";
+let MainGrid = document.querySelector('#MainContainer');
+let UserInput = document.querySelector('#input');
+let ApplyUserInput = document.querySelector('#apply');
 
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
+let color = document.querySelector('#color');
+let erase = document.querySelector('#erase');
+let reset = document.querySelector('#reset');
+
+let DefaultGrid = 8;
+
+MotherSquare(DefaultGrid);
+
+//make the SquareElements
+function ChildSquare(size) {
+    let Square = document.createElement('div');
+    Square.classList.add('box') // EDIT THIS IN CSS YOU MOTHERFUCKER!!!!
+    Square.style.width = `${size}px`;
+    Square.style.width = `${size}px`;
+
+    return Square;
 }
+
+//making the grid algorithm and putting the squares in the maingrid
+function MotherSquare(size){
+    ApplyUserInput.addEventListener('click', () => {
+        let number = UserInput.value;
+        if(number < 2){
+            alert("ERROR!, number to low!");
+        }else if(number > 100){
+            alert("ERROR! number to big!");
+        }else if(isNaN(number)){
+            alert('make sure you put in a number!');
+        }else{
+            for(let a = 0; a < size; a++) {
+                for(let b = 0; b < size; b++) {
+                    //here you append the square to the maingrid!
+                    MainGrid.appendChild(ChildSquare(MainGrid.clientWidth / size));
+                }
+            }
+        }
+    });
+}
+
+//removes ChildSquare from MainGrid
+function ResetGrid() {
+    while(MainGrid.firstChild) {
+        MainGrid.removeChild(MainGrid.lastChild);
+    }
+    MotherSquare(ChildSquare);
+}
+
+// Used event delegation to target children of the grid
+MainGrid.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "box" class
+    if (e.target.matches('.box')) {
+      e.target.classList.add('active');
+    }
+});
+
+//remove ChildSquare from MotherSquare
+reset.addEventListener('click', () => {
+    ResetGrid();
+});
