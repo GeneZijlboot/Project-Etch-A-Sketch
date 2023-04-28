d502b9adaca41b8b7c5fcaed90e0707c0ca6549d
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-04-28 18:36:01 +0200

    new looks

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-//declaring variables
-const grid = document.querySelector('MainContainer');
-let squareSize = 8;
+let board = document.querySelector("MainContainer");
+board.style.gridTemplateColumns =  "repeat(16 , 1fr)";
+board.style.gridTemplateRows =  "repeat(16 , 1fr)";
 
-GeneratingGrid(squareSize);
-
-//creating squared divs
-function SquareMaker(size){
-    const div = document.createElement('div')
-    div.classList.add('box');
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-
-    return div;
-}
-
-//creating the grid
-function GeneratingGrid() {
-
-    //declaring variables
-    const button = document.getElementById('button');
-    let userinput = document.getElementById('input'); 
-
-    //declaring error message for less then 2 or bigger then 100
-    button.addEventListener("click", () => {
-        if(userinput.value < 2){
-            alert("ERROR, number to low");
-        }else if(userinput.value > 100){
-            alert("ERROR! number to big");
-        }else{
-            //creating the loop for building the grid
-            for(let x = 0; x < userinput.value; x++)
-            {   
-                for(let y = 0; y < userinput.value; y++)
-                {
-                   console.log("sure fam!");
-                   grid.appendChild(SquareMaker(grid.clientWidth / userinput.value)); //?
-                }
-            }
-        }
-    });
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
 }
