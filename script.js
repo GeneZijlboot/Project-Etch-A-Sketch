9bb9bba815f9efd635c3b9e2f0d3c0317d1eadb9
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-05-01 23:11:17 +0200

    final version of Project: Etch a Sketch

--- script.js
+++ script.js
@@ -3,32 +3,33 @@ let UserInput = document.querySelector('#input');
 let ApplyUserInput = document.querySelector('#apply');
 
 let color = document.querySelector('#color');
-let erase = document.querySelector('#erase');
-let reset = document.querySelector('#reset');
+let eraseBtn = document.querySelector('#erase');
+let resetBtn = document.querySelector('#reset');
 
-let DefaultGrid = 8;
-
-MotherSquare(DefaultGrid);
-
+MotherSquare();
 //make the SquareElements
 function ChildSquare(size) {
     let Square = document.createElement('div');
     Square.classList.add('box') // EDIT THIS IN CSS YOU MOTHERFUCKER!!!!
     Square.style.width = `${size}px`;
     Square.style.width = `${size}px`;
-
+    resetBtn.addEventListener('click', () => {
+        Square.remove();
+        ApplyUserInput.disabled = false;
+    });
     return Square;
 }
 
 //making the grid algorithm and putting the squares in the maingrid
 function MotherSquare(size){
     ApplyUserInput.addEventListener('click', () => {
-        let number = UserInput.value;
-        if(number < 2){
+        size = 0;
+        size = UserInput.value;
+        if(size < 2){
             alert("ERROR!, number to low!");
-        }else if(number > 100){
+        }else if(size > 100){
             alert("ERROR! number to big!");
-        }else if(isNaN(number)){
+        }else if(isNaN(size)){
             alert('make sure you put in a number!');
         }else{
             for(let a = 0; a < size; a++) {
@@ -38,15 +39,8 @@ function MotherSquare(size){
                 }
             }
         }
+        ApplyUserInput.disabled = true;
     });
-}
-
-//removes ChildSquare from MainGrid
-function ResetGrid() {
-    while(MainGrid.firstChild) {
-        MainGrid.removeChild(MainGrid.lastChild);
-    }
-    MotherSquare(ChildSquare);
 }
 
 // Used event delegation to target children of the grid
@@ -55,9 +49,4 @@ MainGrid.addEventListener('mouseover', function (e) {
     if (e.target.matches('.box')) {
       e.target.classList.add('active');
     }
-});
-
-//remove ChildSquare from MotherSquare
-reset.addEventListener('click', () => {
-    ResetGrid();
 });
